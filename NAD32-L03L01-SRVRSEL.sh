938435af47b68b4aa006450ed1eb3d83b5489991
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-04 14:39:15 -0700

    Update NAD32-L03L01-SRVRSEL.sh

--- NAD32-L03L01-SRVRSEL.sh
+++ NAD32-L03L01-SRVRSEL.sh
@@ -9,13 +9,5 @@ PS4='+$(date +"%T.%3N"): '
 
 # update nginx host for the specific lab
 
-sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application1
-sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application2
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application1/app1.html
-sudo scp /tmp/app1.html                                    nginx:/home/ubuntu/public_html/application1/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application2/app2.html
-sudo scp /tmp/app1.html                                    nginx:/home/ubuntu/public_html/application2/
-
-sudo ssh nginx mkdir --parents /home/ubuntu/public_html/shop/product/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/shop/product/product{1,2,3}.html
-sudo scp /tmp/product*.html                                nginx:/home/ubuntu/public_html/shop/product/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/default.bak
+sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
