9722e380f6441d695dad451819c9e318c39451a6
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-04 15:43:20 -0700

    Update NAD32-L11L01-MONMNGSYS.sh

--- NAD32-L11L01-MONMNGSYS.sh
+++ NAD32-L11L01-MONMNGSYS.sh
@@ -2,20 +2,21 @@
 set -x
 PS4='+$(date +"%T.%3N"): '
 
-# update Student Workstation host for the specific lab
-# only if need a simpler /etc/hosts file do the following 2 steps
-# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab01/hosts
-# sudo cp /tmp/hosts                                  /etc/hosts
-
 # update nginx host for the specific lab
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/default.bak
+sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
-sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application1
-sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application2
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application1/app1.html
-sudo scp /tmp/app1.html                                    nginx:/home/ubuntu/public_html/application1/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application2/app2.html
-sudo scp /tmp/app1.html                                    nginx:/home/ubuntu/public_html/application2/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab11/mywebserver.bak
+sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/
 
-sudo ssh nginx mkdir --parents /home/ubuntu/public_html/shop/product/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/shop/product/product{1,2,3}.html
-sudo scp /tmp/product*.html                                nginx:/home/ubuntu/public_html/shop/product/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/return_test.bak
+sudo scp /tmp/return_test.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab06/variable_test.bak
+sudo scp /tmp/variable_test.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab11/sampleapp.bak
+sudo scp /tmp/sampleapp.bak                              nginx:/etc/nginx/conf.d/sampleapp.bak
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab11/ssl-params.bak
+sudo scp /tmp/ssl-params.bak                              nginx:/etc/nginx/conf.d/
