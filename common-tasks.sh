9f657afd436979b3ea5e1d686cee3237338d2b23
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-04 14:03:42 -0700

    Update common-tasks.sh

--- common-tasks.sh
+++ common-tasks.sh
@@ -20,6 +20,28 @@ echo Exec=google-chrome --app=https://f5.bravais.com/s/$brav_id >>/home/student/
 gio set /home/student/Desktop/Lab_Guide.desktop metadata::trusted true
 chmod +x /home/student/Desktop/Lab_Guide.desktop
 
+# common files for all labs
+sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application1
+sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application2
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/index.html
+sudo scp /tmp/index.html                                   nginx:/home/ubuntu/public_html/index.html
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application1/app1.html
+sudo scp /tmp/app1.html                                    nginx:/home/ubuntu/public_html/application1/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application2/app2.html
+sudo scp /tmp/app2.html                                    nginx:/home/ubuntu/public_html/application2/
+
+sudo ssh nginx mkdir --parents /home/ubuntu/public_html/shop/product
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/shop/product/product{1,2,3}.html
+sudo scp /tmp/product*.html                                nginx:/home/ubuntu/public_html/shop/product/
+
+sudo ssh nginx mkdir --parents /data/images
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/data/images/logo.png
+sudo scp /tmp/logo.png                                    nginx:/data/images/
+
+sudo ssh nginx mkdir --parents /data/server2/sampleapp
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/data/server2/sampleapp/index.html
+sudo scp /tmp/index.html                                  nginx:/data/server2/sampleapp/
+
 # run this lab's specific tasks saved on GitHub
 curl --silent --output /tmp/$LAB_ID.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$LAB_ID.sh
 bash -x /tmp/$LAB_ID.sh
