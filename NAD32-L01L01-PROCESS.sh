c0ac3afa06d52c187dee425a50f954aa5fea0f56
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-04 10:47:29 -0700

    Update NAD32-L01L01-PROCESS.sh

--- NAD32-L01L01-PROCESS.sh
+++ NAD32-L01L01-PROCESS.sh
@@ -8,4 +8,14 @@ PS4='+$(date +"%T.%3N"): '
 # sudo cp /tmp/hosts                                  /etc/hosts
 
 # update nginx host for the specific lab
-sudo ssh nginx mkdir --parents /home/ubuntu/public_html
+
+sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application1
+sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application2
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application1/app1.html
+sudo scp /tmp/app1.html                                    nginx:/home/ubuntu/public_html/application1/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/application2/app2.html
+sudo scp /tmp/app1.html                                    nginx:/home/ubuntu/public_html/application2/
+
+sudo ssh nginx mkdir --parents /home/ubuntu/public_html/shop/product/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/common/ubuntu/public_html/shop/product/product{1,2,3}.html
+sudo scp /tmp/product*.html                                nginx:/home/ubuntu/public_html/shop/product/
