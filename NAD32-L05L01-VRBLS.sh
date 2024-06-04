481c49ddbfa4c7da2a4c783f6e11536977110bd1
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-04 14:51:26 -0700

    Update NAD32-L05L01-VRBLS.sh

--- NAD32-L05L01-VRBLS.sh
+++ NAD32-L05L01-VRBLS.sh
@@ -7,8 +7,8 @@ PS4='+$(date +"%T.%3N"): '
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/default.bak
 sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/mywebserver.bak
-sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/mywebserver.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab05/mywebserver.bak
+sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/
 
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/return_test.bak
 sudo scp /tmp/return_test.bak                              nginx:/etc/nginx/conf.d/
