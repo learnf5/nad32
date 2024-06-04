0a37e81d54df3548dd95e84f06c187fcfb3dc629
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-03 18:34:21 -0700

    Update and rename nad32-L01L01-Proces.sh to NEW-nad32-L01L01-Proces.sh

---
+++ NEW-nad32-L01L01-Proces.sh
@@ -0,0 +1,12 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update Student Workstation host for the specific lab
+# only if need a simpler /etc/hosts file do the following 2 steps
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab01/hosts
+# sudo cp /tmp/hosts                                  /etc/hosts
+
+# update nginx host for the specific lab
+# NOTHING special for Lab1
+
