4631b9f796ed87e76ab2536c1208ff85e2599de6
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-04 14:54:51 -0700

    Update NAD32-L06L01-LOGGING.sh

--- NAD32-L06L01-LOGGING.sh
+++ NAD32-L06L01-LOGGING.sh
@@ -1,1 +1,15 @@
 
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update nginx host for the specific lab
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/default.bak
+sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab05/mywebserver.bak
+sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/return_test.bak
+sudo scp /tmp/return_test.bak                              nginx:/etc/nginx/conf.d/
