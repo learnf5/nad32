c93e3d08cde9d2760569cce1d7f1957ce563c02f
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-26 16:54:25 -0700

    Update NAD32-L06L01-LOGGING.sh

--- NAD32-L06L01-LOGGING.sh
+++ NAD32-L06L01-LOGGING.sh
@@ -5,6 +5,9 @@ PS4='+$(date +"%T.%3N"): '
 
 # update nginx host for the specific lab
 
+#Remove default.conf file since this lab requires only default.bak
+sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/default.bak
 sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
